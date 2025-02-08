60e4cb9d44345f272ca8be4bb96f46240111665a
Nicholas Lamicela <nupanick@gmail.com>
2025-02-08 15:30:20 -0800

    random plates

--- plates/index.js
+++ plates/index.js
@@ -5,9 +5,29 @@ function main() {
 
 function generate() {
   const mainDiv = document.getElementById('main');
-  mainDiv.innerHTML += '<p>ABC 123</p>';
-  mainDiv.innerHTML += '<p>DEF 456</p>';
-  mainDiv.innerHTML += '<p>GHI 789</p>';
+  for (let i = 0; i < 3; i += 1) {
+    mainDiv.innerHTML += '<p>' + randomPlate() + '</p>';
+  }
+}
+
+// no O, I, Q, or Z
+const letters = 'ABCDEFGHJKLMNPRSTUVWXY';
+
+// no 0
+const numbers = '123456789'
+
+function randomPlate() {
+  const chars = [];
+  for (let i = 0; i < 3; i += 1) {
+    const n = Math.floor(Math.random() * letters.length);
+    chars.push(letters[n]);
+  }
+  chars.push(' ');
+  for (let i = 0; i < 3; i += 1) {
+    const n = Math.floor(Math.random() * numbers.length);
+    chars.push(numbers[n]);
+  }
+  return chars.join('');
 }
 
 main();
