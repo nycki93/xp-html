039139cf02ceb84d3fd8602a3c4fe172644d6a4b
Nicholas Lamicela <nupanick@gmail.com>
2025-02-08 15:17:46 -0800

    generate three (dummy) plates

--- plates/index.js
+++ plates/index.js
@@ -1,6 +1,13 @@
 function main() {
+  const generateButton = document.getElementById('generate');
+  generateButton.onclick = generate;
+}
+
+function generate() {
   const mainDiv = document.getElementById('main');
-  mainDiv.innerHTML += '<h2>Hello from JS</h2>';
+  mainDiv.innerHTML += '<p>ABC 123</p>';
+  mainDiv.innerHTML += '<p>DEF 456</p>';
+  mainDiv.innerHTML += '<p>GHI 789</p>';
 }
 
 main();
